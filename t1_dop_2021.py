3d6c15011cac3084691be4dd75297c5162208ec8
Somebodyonc3 <91481770+Somebodyonc3@users.noreply.github.com>
2021-12-06 11:58:45 +0000

    No time gotta move

---
+++ t1_dop_2021.py
@@ -0,0 +1,23 @@
+from itertools import product
+
+print(list(product('1234', repeat = 4)))
+
+L = input()
+S = input()
+t1 = input()
+t2 = input()
+t3 = input()
+t4 = input()
+
+shifr = []
+shifr.append(t1)
+shifr.append(t2)
+shifr.append(t3)
+shifr.append(t4)
+
+for _ in range(0, len(L)):
+  x = L[i]
+  for i in range(len(shifr)):
+    for x in shifr:
+      if x in 
+
