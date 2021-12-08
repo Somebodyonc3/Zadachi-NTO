7f41242cc5f060cac99ea0766a96fdfc62a456c3
Somebodyonc3 <91481770+Somebodyonc3@users.noreply.github.com>
2021-12-08 16:34:10 +0000

    ez

--- t1_dop_2021.py
+++ t1_dop_2021.py
@@ -1,23 +1,35 @@
-from itertools import product
+s1 = 'horse'
+s2 = 'ubjsd'
+p = [#1234 => 0123
+    #abcdefghijklmnopqrstuvwxyz
+    'qwertyuiopasdfghjklzxcvbnm',
+    'qwertyuiopasdfghjklzxcvbnm',
+    'qwertyuiopasdfghjklzxcvbnm',
+    'qwertyuiopasdfghjklzxcvbnm']
 
-print(list(product('1234', repeat = 4)))
+import itertools
+allp = list(itertools.product('0123', repeat=4))
 
-L = input()
-S = input()
-t1 = input()
-t2 = input()
-t3 = input()
-t4 = input()
+ans = set()
 
-shifr = []
-shifr.append(t1)
-shifr.append(t2)
-shifr.append(t3)
-shifr.append(t4)
-
-for _ in range(0, len(L)):
-  x = L[i]
-  for i in range(len(shifr)):
-    for x in shifr:
-      if x in 
+for x in allp[:4]:
+    t1 = p[int(x[0])]
+    t2 = p[int(x[1])]
+    t3 = p[int(x[2])]
+    t4 = p[int(x[3])]
+    letter1 = s1[0]
+    t1let = ord(letter1) - ord('a')
+    letter2 = t1[t1let]
+    t1let = ord(letter2) - ord('a')
+    letter3 = t2[t1let]
+    t1let = ord(letter3) - ord('a')
+    letter4 = t3[t1let]
+    t1let = ord(letter4) - ord('a')
+    letter5 = t4[t1let]
+    if letter1 == letter5:
+        ans.add((t1, t2, t3, t4))
+    else:
+        ans -= (t1, t2, t3, t4)
+        continue
+    print(letter1, letter2, letter3, letter4, letter5)
 
