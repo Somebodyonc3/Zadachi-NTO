f20cdaeac81f3e4f8ddd0a9bc5d2b27aa681b862
Somebodyonc3 <91481770+Somebodyonc3@users.noreply.github.com>
2021-12-17 18:57:55 +0000

    ьььььь

--- t2_k_2021.py
+++ t2_k_2021.py
@@ -1,4 +1,7 @@
-q = 1
+q = 2
+
+Answer = []
+
 for z in range(q):
 
     dic = {1:[], 2:[], 3:[]}
@@ -14,30 +17,31 @@ for z in range(q):
 
 
     if len(dic[3]) > 0:
-        print(f'{3 - dic[3][1]},{3 - dic[3][0]}')
+        Answer.append(f'{3 - dic[3][1]},{3 - dic[3][0]}')
     elif len(dic[2]) > 0:
         t = dic[2][0]
         if t[0] == 0:
-            print(f'{3 - t[1]},4')
+            Answer.append(f'{3 - t[1]},4')
         elif t[0] == 6:
-            print(f'{3 - t[1]},-4')
+            Answer.append(f'{3 - t[1]},-4')
         elif t[1] == 0:
-            print(f'4,{3 - t[0]}')
+            Answer.append(f'4,{3 - t[0]}')
         else:
-            print(f'-4,{3 - t[0]}')
+            Answer.append(f'-4,{3 - t[0]}')
 
     else:
         t = dic[1][0]
         if t[0] == 0:
-            print(f'{3 - t[1]},5')
+            Answer.append(f'{3 - t[1]},5')
         elif t[0] == 6:
-            print(f'{3 - t[1]},-5')
+            Answer.append(f'{3 - t[1]},-5')
         elif t[1] == 0:
-            print(f'5,{3 - t[0]}')
+            Answer.append(f'5,{3 - t[0]}')
         else:
-            print(f'-5,{3 - t[0]}')
+            Answer.append(f'-5,{3 - t[0]}')
     if z < q - 1:
         input()
+print(*Answer, sep = '\n')
 
 #print(dic)
 #{1: [(1, 3), (5, 3)], 2: [(2, 3), (4, 3), (3, 2), (3, 4)], 3: (3, 3)}
