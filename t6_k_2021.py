fe4fc267582801bd6944c31d4813f45ded90709a
Somebodyonc3 <91481770+Somebodyonc3@users.noreply.github.com>
2022-01-07 20:25:37 +0000

    fffffffff

--- t6_k_2021.py
+++ t6_k_2021.py
@@ -3,176 +3,181 @@
 
 Воодушевлённый возможностью решить нерешаемую задачу, Винтик взялся за дело. Он построил специальную печь с управляемым нагревом, а чтобы точно выдержать температурный режим и исключить влияние даже сквозняка — подключил к ней ПИД-регулятор. Также он провёл испытания печи и построил её математическую модель. Однако его экзистенциальная несовместимость с яйцами всмятку снова проявила себя в том, что у него никак не получается подобрать параметры ПИД-регулятора! В прошлом он решал такие задачи тысячи раз, он может подобрать эти три несчастных числа для чего угодно, но только не для яиц всмятку. В отчаянии он выложил все параметры задачи на форум, включая матмодель печи, описание ПИД-регулятора и даже матмодель сквозняков! Помогите ему, подобрав параметры ПИД-регулятора для его задачи до того, как он решит, что яйца всмятку — мировой заговор и их не существует.
 
-Код, содержащий матмодели, можно скачать по ссылке.
-
 Входной формат: единственное целое число — затравка генератора сквозняка (см. код).
 
 Выходной формат: три числа через пробел — коэффициенты P, I и D регулятора.
 '''
 
+"""
+Проект: Аккуратный Нагрев в Условиях Сквозняка
+(С) 2021 Инженер Винтик
+Лицензия: MyPrecious
+"""
 import random
-from decimal import *
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
-import os
 
-# SOURCE: http://blog.nikmartin.com/2012/11/process-control-for-dummies-like-me.html
+# TODO: портировать под квантовый суперкомпьютер
 
-y_data = []
-x_data = []
-error_value_data = []
-current_value_data = []
-controller_output_data = []
-proportional_value_data = []
-integral_value_data = []
-derivative_value_data = []
-setpoint_value_data = []
-process_value_data = []
+maxTime = 1000  # количество отсчётов
+dt = 1  # интервал отсчётов (в секундах)
+minVal, maxVal = -1000, 1000  # интервал допустимых управляющих воздействий
+integral, prevErr = 0, 0
 
-OUTGOING_FILES_DIRECTORY = "/some/dir/in/your/computer"
+#########
+SEED = 410915411 # затравка для генератора сквозняков
 
-def create_offset(setpoint_value):
-    offset_value = random.randint(-128, 128)
-    offset_value_incrementation = float(offset_value / 100)
-    return setpoint_value - offset_value_incrementation
+# TODO: подобрать коэффициенты (я кушать хочу :( )
+KP = 1.0
+KI = 0.0
+KD = 0.0
+##########
+
+def rand_wind(n, seed=None):
+    """Генерирует график сквозняков.
+
+    n — количество отсчётов
+    seed — затравка для генератора случайных чисел
+    """
+    if seed:
+        random.seed(seed)
+    a = []
+    x = random.uniform(-5, 5)
+    dx = 0
+    dt = 0
+    while len(a) < n:
+        if dt == 0:            
+            mode = random.randint(1, 3)
+            sgn = random.choice((-1, 1))
+            if mode == 1:
+                dt = random.randint(20, 30)
+                dx = sgn * random.uniform(2, 3) / 10
+            elif mode == 2:
+                dt = random.randint(10, 20)
+                dx = sgn * random.uniform(3, 5) / 10
+            elif mode == 3:
+                dt = random.randint(5, 10)
+                dx = sgn * random.uniform(5, 10) / 10
+        a.append(x)
+        x += dx
+        dt -= 1
+    return a
+
+external = rand_wind(maxTime, seed=SEED)
+steadyT, activeT = 150, 300
+setpoint = (
+    [25 + x/250 * (steadyT-25) for x in range(250)] +
+    [steadyT for x in range(250)] +
+    [steadyT + x/150 * (activeT-steadyT) for x in range(150)] +
+    [activeT for x in range(maxTime-800)] +
+    [activeT - x/150 * (activeT-steadyT) for x in range(150)]
+)
 
 
-def Pt_controller(process_error_value, Pgain_value):
-    Pt = process_error_value * Pgain_value
-    return Pt
+def pid_compute(target, setpoint):
+    """ Вычисляет управляющее воздействие нагревателя.
+
+    target — текущая температура в печи
+    setpoint — необходимая температура
+    """
+
+    """
+    TODO: реализовать ПИД-регулятор по формуле
+    k(t) = kp * (e(t) + ki * dt * sum(e(t)) + kd * (e(t)-e(t-1)) / dt)
+    Интегральная часть не ограничивается.
+    А вот выходное значение — ограничивается!
+    """
+
+    return 0
 
 
-def It_controller(It, process_error_value, Igain_value):
-    It = It + process_error_value
-    It = It * Igain_value
-    return It
+def calc(externals, targets, controls, newControl):
+    """ Вычисляет новую температуру печи.
 
+    externals – история температуры сквозняка (в т.ч. текущее значение)
+    targets – история температуры печи (в т.ч. текущее значение)
+    controls – предыдущие значения из ПИД-регулятора
+      (во всех списках последнее значение — справа)
+    newControl – текущее значение ПИД-регулятора
 
-def Dt_controller(current_control_output_value, previous_control_output_value, derivative_gain_value):
-    Dt = (current_control_output_value - previous_control_output_value) * derivative_gain_value
-    # print(current_control_output_value, previous_control_output_value, Dt)
-    return Dt
+    Изобретения хорошего инженера законам реальности не подчиняются,
+    а любая достаточно продвинутая технология неотличима от магии.
+    """
+    # Хвост по обратной связи
+    tf = 0
+    n = 0
+    trailLength = 10
+    trailWidth = 0.9
+    for i in range(min(trailLength,len(targets))):
+        scale = trailWidth ** i
+        tf += externals[-(i+1)] * scale
+        n += scale
+    tf /= n
 
-def process_error(setpoint_value, current_value):
-    return setpoint_value - current_value
+    # Хвост по внешнему воздействию
+    te = 0
+    nc = 0
+    for i in range(min(trailLength,len(externals))):
+        scale = trailWidth ** i
+        te += externals[-(i+1)] * scale
+        nc += scale
+    te /= nc + 1
+    
+    # Возведение в степень
+    def pw(x,p):
+        if x>0:
+            return x ** p
+        else:
+            return -(abs(x) ** p)
 
-def pid_controller(proportional_value, integral_value, derivative_value):
-    pid_output = derivative_value + (proportional_value + integral_value)
-    return pid_output
-
-def graph_line_data(x_value, error_value, proportional_value, integral_value, derivative_value, setpoint_value, process_value, PID_value):
-
-    if max(x_value) > 500:
-        width = max(x_value)/200
+    # Как-бы производная по внешнему воздействию
+    if len(externals) < 2:
+        de = 0
     else:
-        width = 10
-    height = 5
-    plt.figure(figsize=(width, height), dpi=100)
+        de = externals[-1] - externals[-2]
+        de = pw(de,1.2)
+        de = de.real
 
-    # Choose which data
-    plt.plot(x_value, PID_value, '#FF4500')
-    # plt.plot(x_value, error_value, 'r')
-    # plt.plot(x_value, proportional_value, '#9F00A0')
-    # plt.plot(x_value, integral_value, 'green')
-    # plt.plot(x_value, derivative_value, '#00BBFF')
-    plt.plot(x_value, setpoint_value, 'k--')
-    # plt.plot(x_value, process_value, 'b')
+    # Как-бы производная по обратной связи
+    if len(targets) < 2:
+        df = 0
+    else:
+        df = targets[-1] - targets[-2]
+        df = pw(df,0.85)
+        df = df.real
+    
+    # Как-бы производная по управлению
+    if len(controls) < 1:
+        dc = 0
+    else:
+        dc = newControl - controls[-1]
+        dc = pw(dc,1.1)
+        dc = dc.real
+    
+    newTarget = ( 0
+        + tf
+        + te
+        + de
+        + df
+        + dc
+        + newControl
+    )
+    return newTarget
 
-    plt.xlabel('Iteration Count')
-    plt.ylabel('Output')
-    plt.title('PID Controller\n(P=0.82, I=0.155, D=0.45)')
-
-    plt.legend(['PID Controller Output', 'Set point'], loc='upper right')
-    # plt.legend(['PID Controller Output'], loc='upper right')
-
-    # plt.legend(['PID Controller Output', 'Error Value', 'Integral Value'], loc='upper right')
-
-    # plt.legend(['PID', 'Error Value', 'Proportional Value', 'Integral Value',
-    #            'Derivative Value', 'Set point', 'Process Value'], loc='upper right')
-
-    png_filename = "Steady_PID_Simulation"
-
-    output_file_path = os.path.join(OUTGOING_FILES_DIRECTORY, png_filename)
-
-    plt.savefig(output_file_path)
-
-    print("Finished creating: " + png_filename + ".png")
-
-
-
-
-def pid_simulator():
-    # set point
-    setpoint_value = 0
-
-    # gain values          # P = 0.82 / I = 0.15 / D = 0.40
-    proportional_gain_value = 0.82
-    integral_gain_value = 0.155
-    derivative_gain_value = 0.45
-
-    # initialisation values
-    derivative_value = 1
-    controller_output_value = 0
-    integral_value = 0
-    x_value = 0
-    number_of_iterations = 200
-
-    while x_value < number_of_iterations:
-        x_value += 1
-
-        process_value = setpoint_value
-
-        error_value = process_value - setpoint_value
-
-        if x_value == number_of_iterations / 5:
-            setpoint_value = 100
-        if x_value == number_of_iterations / 2:
-            setpoint_value = -300
-        if x_value == 6 * (number_of_iterations / 10):
-            setpoint_value = -200
-        if x_value == 7 * (number_of_iterations / 10):
-            setpoint_value = -250
-        if x_value == (number_of_iterations - (number_of_iterations / 5)):
-            setpoint_value = 0
-
-        # PROPORTIONAL
-        proportional_value = Pt_controller(process_value, proportional_gain_value)
-
-        # INTEGRAL
-        integral_value = It_controller(integral_value, process_value, integral_gain_value)
-
-        # CONTROLLER OUTPUT
-        previous_controller_output_value = controller_output_value
-        controller_output_value = proportional_value + integral_value + derivative_value
-
-        # DERIVATIVE
-        derivative_value = Dt_controller(controller_output_value, previous_controller_output_value, derivative_gain_value)
-        # derivative_value = 0.25 * derivative_value
-
-        # print(integral_value)
-        # print(number_of_iterations - x_value)
-        x_data.append(x_value)
-        controller_output_data.append(controller_output_value)
-        error_value_data.append(error_value)
-        integral_value_data.append(integral_value)
-        setpoint_value_data.append(setpoint_value)
-        process_value_data.append(process_value)
-        derivative_value_data.append(derivative_value)
-        proportional_value_data.append(proportional_value)
-
-    graph_line_data(x_data, error_value_data, proportional_value_data, integral_value_data, derivative_value_data,
-                    setpoint_value_data, process_value_data, controller_output_data)
-
-
-
-
-def main():
-    pid_simulator()
-
-
-
-if __name__ == "__main__":
-    main()
+def get_targets(data=None):
+    """ Считает график температуры внутри печи.
+    """
+    if data is not None:
+        global KP, KI, KD, integral, prevErr
+        integral, prevErr = 0, 0
+        KP, KI, KD = data
+    
+    targets, externals, controls = [], [], []
+   
+    value = 25
+    for (set_p, ext) in zip(setpoint, external):
+        externals.append(ext)
+        targets.append(value)
+        newControl = pid_compute(value, set_p)
+        controls.append(newControl)
+        value = calc(externals, targets, controls, newControl)
+    
+    return targets, controls
